450ccc38b769a5cdb2dbd8f583dac51af3dd4477
Tyler Richards <tyler@tyler-richards.com>
2021-07-08 19:05:32 -0400

    updated. TODO collison

--- snake.py
+++ snake.py
@@ -1,6 +1,46 @@
 # -*- coding: utf-8 -*-
 import curses
 import locale
+import random
+
+BLOCK = u"\u2588"
+
+class Node:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+
+def draw_apple(stdscr, snake):
+    coords = []
+    height, width = stdscr.getmaxyx()
+    for i in range(height-2):
+        for j in range(width-2):
+            coords.append((i+1,j+1))
+    snakepoints=snake_points(snake)
+    loc = random.choice([i for i in coords if i not in snakepoints])
+    return (loc[0], loc[1])
+
+def snake_points(snake):
+    snakepoints=[]
+    while snake.next != None:
+        snakepoints.append(snake.data)
+        snake = snake.next
+    return snakepoints
+
+def draw_snake(stdscr, snake, data, head=False):
+
+
+    y, x = data
+    stdscr.addstr(y, x, BLOCK)
+    nextdata = snake.data
+    snake.data = (y,x)
+
+    if snake.next == None:
+        return True
+
+
+    draw_snake(stdscr, snake.next, nextdata)
 
 def draw_game(stdscr):
 
@@ -9,9 +49,13 @@ def draw_game(stdscr):
     cursor_x = width// 2
     cursor_y = height //2
 
+    snake = Node((cursor_y,cursor_x))
+
+    apple = draw_apple(stdscr, snake)
     # Clear and refresh the screen for a blank canvas
     stdscr.erase()
     stdscr.refresh()
+    curses.curs_set(0)
 
     # Start colors in curses
     curses.start_color()
@@ -23,7 +67,7 @@ def draw_game(stdscr):
     while (k != ord('q')):
         frame += 1
         # Initialization
-        stdscr.erase()
+        stdscr.clear()
         height, width = stdscr.getmaxyx()
 
         if k == curses.KEY_DOWN:
@@ -35,6 +79,7 @@ def draw_game(stdscr):
         elif k == curses.KEY_LEFT:
             cursor_x = cursor_x - 1
 
+
         cursor_x = max(1, cursor_x)
         cursor_x = min(width - 2, cursor_x)
 
@@ -42,21 +87,32 @@ def draw_game(stdscr):
         cursor_y = min(height - 2, cursor_y)
 
 
-        stdscr.addstr(1, 1, "frame:{}".format(frame))
-        stdscr.addstr(2, 1, "key:{}".format(k))
-        stdscr.addstr(3, 1, "char:{}".format(chr(219)))
+        #stdscr.addstr(1, 1, "frame:{}".format(frame))
+        #stdscr.addstr(2, 1, "key:{}".format(k))
+        #stdscr.addstr(3, 1, "char:{}".format(BLOCK))
 
         stdscr.attron(curses.color_pair(1))
         stdscr.border();
         stdscr.attroff(curses.color_pair(1))
 
         stdscr.move(cursor_y, cursor_x)
+        if (cursor_y, cursor_x) == apple:
+            snake = Node((cursor_y, cursor_x), snake)
+            apple = draw_apple(stdscr,snake)
+        sp = snake_points(snake)
+
+        draw_snake(stdscr, snake, (cursor_y,cursor_x), True)
+
+        if sp.count((cursor_y,cursor_x)) > 0:
+            exit()
+
+        stdscr.addstr(apple[0], apple[1], "*")
 
         # Refresh the screen
         stdscr.refresh()
 
         # Wait for next input
-        curses.halfdelay(5)
+        curses.halfdelay(1)
         last=k
         k = stdscr.getch()
         if k == -1:
