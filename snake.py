a6089d92dddc704f9d1e67dd62ba15a7c2610d05
Tyler Richards <tyler@tyler-richards.com>
2021-07-08 21:40:30 -0400

    Added comments.

--- snake.py
+++ snake.py
@@ -5,22 +5,26 @@ import random
 
 BLOCK = u"\u2588"
 
+
+#Snake sections (Also a super cool linked list node)
 class Node:
     def __init__(self, data=None, next=None):
         self.data = data
         self.next = next
 
-
+#Draws apple in random location that is not snake related
 def draw_apple(stdscr, snake):
     coords = []
     height, width = stdscr.getmaxyx()
     for i in range(height-2):
         for j in range(width-2):
-            coords.append((i+1,j+1))
+            coords.append((i+1,j+1)) #This is a mess and needs work
     snakepoints=snake_points(snake)
     loc = random.choice([i for i in coords if i not in snakepoints])
     return (loc[0], loc[1])
 
+
+#Returns a list of all current locations of the snakes body
 def snake_points(snake):
     snakepoints=[]
     while snake.next != None:
@@ -28,9 +32,8 @@ def snake_points(snake):
         snake = snake.next
     return snakepoints
 
+#draws updated snake on screen
 def draw_snake(stdscr, snake, data, head=False):
-
-
     y, x = data
     stdscr.addstr(y, x, BLOCK)
     nextdata = snake.data
@@ -39,37 +42,49 @@ def draw_snake(stdscr, snake, data, head=False):
     if snake.next == None:
         return True
 
-
     draw_snake(stdscr, snake.next, nextdata)
 
+#draws game board
 def draw_game(stdscr):
 
     k = 0
-    height, width = stdscr.getmaxyx()
+    height, width = stdscr.getmaxyx() # gets max terminal size
+
+    #centers snake
     cursor_x = width// 2
     cursor_y = height //2
 
+    #init snake with single node
     snake = Node((cursor_y,cursor_x))
 
+    #init first apple location
     apple = draw_apple(stdscr, snake)
-    # Clear and refresh the screen for a blank canvas
+
+    # erase and refresh the screen for a blank canvas
     stdscr.erase()
     stdscr.refresh()
+
+    #turns cursor off
     curses.curs_set(0)
 
     # Start colors in curses
     curses.start_color()
     curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_WHITE)
-
     curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
+
+    #frame counter
     frame = 0
 
     while (k != ord('q')):
         frame += 1
+
+
         # Initialization
-        stdscr.clear()
+        stdscr.clear() #look into performance. redrawing screen can lead to flickering sometimes
         height, width = stdscr.getmaxyx()
 
+
+        #get key presses. TODO: update so pressing a random key doesn't stop the snake
         if k == curses.KEY_DOWN:
             cursor_y = cursor_y + 1
         elif k == curses.KEY_UP:
@@ -80,35 +95,40 @@ def draw_game(stdscr):
             cursor_x = cursor_x - 1
 
 
+        #Set boundaries for snake inside the border
         cursor_x = max(1, cursor_x)
         cursor_x = min(width - 2, cursor_x)
-
         cursor_y = max(1, cursor_y)
         cursor_y = min(height - 2, cursor_y)
 
+        #Collision detection
         sp = snake_points(snake)
         if sp.count((cursor_y,cursor_x)) > 0:
             exit()
 
+        #Sets border
         stdscr.attron(curses.color_pair(1))
         stdscr.border();
         stdscr.attroff(curses.color_pair(1))
 
+        #set location of cursor
         stdscr.move(cursor_y, cursor_x)
-        if (cursor_y, cursor_x) == apple:
-            snake = Node((cursor_y, cursor_x), snake)
-            apple = draw_apple(stdscr,snake)
 
+        #Apple detection
+        if (cursor_y, cursor_x) == apple:
+            snake = Node((cursor_y, cursor_x), snake) #add onto the snake
+            apple = draw_apple(stdscr,snake) #draw new apple
+
+        #Draw snake
         draw_snake(stdscr, snake, (cursor_y,cursor_x), True)
 
-
-
+        #Draw apple
         stdscr.addstr(apple[0], apple[1], "*")
 
         # Refresh the screen
         stdscr.refresh()
 
-        # Wait for next input
+        # set to continue without input
         curses.halfdelay(1)
         last=k
         k = stdscr.getch()
